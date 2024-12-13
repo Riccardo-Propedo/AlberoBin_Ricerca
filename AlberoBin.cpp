f4e9e68cf71024516f81be346d0b9d6c739cc663
Riccardo-Propedo <riccardopropedo007@gmail.com>
2024-12-13 11:30:43 +0100

    AlberoBin.cpp

--- AlberoBin.cpp
+++ AlberoBin.cpp
@@ -1,135 +1,118 @@
 #include <iostream>
+using namespace std;
 
 const int MAX_NODI = 100; // Numero massimo di nodi nell'albero
-const int NODO_VUOTO = -1; // Costante per indicare un figlio assente
+const int NODO_VUOTO = -1; // Costante per indicare un nodo assente
 
-template <typename T>
-class AlberoBin {
+struct Nodo {
+    int valore;
+    int sinistro; // Indice del figlio sinistro
+    int destro;   // Indice del figlio destro
+};
+
+class AlberoBinarioRicerca {
 private:
-    struct Nodo {
-        int sx = NODO_VUOTO;      // Indice del figlio sinistro
-        T valore;    // Valore del nodo
-        int dx = NODO_VUOTO;      // Indice del figlio destro
-    };
-
-    Nodo nodi[MAX_NODI]; // Matrice dei nodi
-    int radiceIndice;    // Indice della radice
-    int prossimoIndice;  // Prossimo indice disponibile
+    Nodo nodi[MAX_NODI]; // Array dei nodi
+    int radice;          // Indice della radice
+    int prossimoIndice;  // Prossimo indice libero
 
 public:
-    // Costruttore
-    AlberoBin() {
+    AlberoBinarioRicerca() {
+        radice = NODO_VUOTO;
         prossimoIndice = 0;
-        radiceIndice = NODO_VUOTO;
     }
 
-    // Verifica se l'albero è vuoto
-    bool alberoVuoto() const {
-        return radiceIndice == NODO_VUOTO;
-    }
-
-    // Inserisce la radice
-    void insRadice(const T& valore) {
-        if (!alberoVuoto()) {
-            std::cout << "Errore: la radice esiste già.\n";
-            return;
-        }
-        nodi[prossimoIndice].valore = valore;
-        radiceIndice = prossimoIndice;
-        prossimoIndice++;
-    }
-
-    // Restituisce il valore della radice
-    T radice() const {
-        if (alberoVuoto()) {
-            std::cout << "Errore: l'albero è vuoto.\n";
-            return T();
-        }
-        return nodi[radiceIndice].valore;
-    }
-
-    // Verifica se un nodo è foglia
-    bool isFoglia(int indice) const {
-        if (indice < 0 && indice > prossimoIndice) {
-            std::cout << "Errore: indice non valido.\n";
-            return false;
-        }
-        return nodi[indice].sx == NODO_VUOTO && nodi[indice].dx == NODO_VUOTO;
-    }
-
-    // Restituisce l'indice del figlio sinistro
-    int figlioSinistro(int indice) const {
-        if (indice < 0 && indice > prossimoIndice) {
-            std::cout << "Errore: indice non valido.\n";
-            return NODO_VUOTO;
-        }
-        return nodi[indice].sx;
-    }
-
-    // Restituisce l'indice del figlio destro
-    int figlioDestro(int indice) const {
-        if (indice < 0 && indice > prossimoIndice) {
-            std::cout << "Errore: indice non valido.\n";
-            return NODO_VUOTO;
-        }
-        return nodi[indice].dx;
-    }
-
-    // Verifica se il figlio sinistro è vuoto
-    bool sinistroVuoto(int indice) const {
-        return figlioSinistro(indice) == NODO_VUOTO;
-    }
-
-    // Verifica se il figlio destro è vuoto
-    bool destroVuoto(int indice) const {
-        return figlioDestro(indice) == NODO_VUOTO;
-    }
-
-    // Inserisce il figlio sinistro
-    void insFiglioSinistro(int indice, const T& valore) {
-        if (indice < 0 && indice > prossimoIndice) {
-            std::cout << "Errore: indice non valido.\n";
-            return;
-        }
-        if (nodi[indice].sx != NODO_VUOTO) {
-            std::cout << "Errore: il figlio sinistro esiste già.\n";
-            return;
-        }
+    void inserisci(int valore) {
         if (prossimoIndice >= MAX_NODI) {
-            std::cout << "Errore: spazio esaurito.\n";
+            cout << "Errore: limite massimo raggiunto.\n";
             return;
         }
-        nodi[prossimoIndice].valore = valore;
-        nodi[indice].sx = prossimoIndice;
-        prossimoIndice++;
-    }
 
-    // Inserisce il figlio destro
-    void insFiglioDestro(int indice, const T& valore) {
-        if (indice < 0 && indice > prossimoIndice) {
-            std::cout << "Errore: indice non valido.\n";
-            return;
-        }
-        if (nodi[indice].dx != NODO_VUOTO) {
-            std::cout << "Errore: il figlio destro esiste già.\n";
-            return;
-        }
-        if (prossimoIndice >= MAX_NODI) {
-            std::cout << "Errore: spazio esaurito.\n";
-            return;
-        }
-        nodi[prossimoIndice].valore = valore;
-        nodi[indice].dx = prossimoIndice;
-        prossimoIndice++;
-    }
-
-    // Stampa l'albero (per debugging)
-    void stampa() const {
-        for (int i = 0; i < prossimoIndice; ++i) {
-            std::cout << "Nodo " << i << ": Valore = " << nodi[i].valore
-                      << ", Figlio Sinistro = " << nodi[i].sx
-                      << ", Figlio Destro = " << nodi[i].dx << "\n";
+        if (radice == NODO_VUOTO) {
+            // L'albero è vuoto: inserisci il primo nodo come radice
+            nodi[prossimoIndice] = {valore, NODO_VUOTO, NODO_VUOTO};
+            radice = prossimoIndice++;
+        } else {
+            // Inserisci ricorsivamente partendo dalla radice
+            inserisciNodo(radice, valore);
         }
     }
 
+    void inserisciNodo(int indice, int valore) {
+        if (valore < nodi[indice].valore) {
+            if (nodi[indice].sinistro == NODO_VUOTO) {
+                // Inserisci come figlio sinistro
+                nodi[prossimoIndice] = {valore, NODO_VUOTO, NODO_VUOTO};
+                nodi[indice].sinistro = prossimoIndice++;
+            } else {
+                // Continua la ricerca nel sottoalbero sinistro
+                inserisciNodo(nodi[indice].sinistro, valore);
+            }
+        } else if (valore > nodi[indice].valore) {
+            if (nodi[indice].destro == NODO_VUOTO) {
+                // Inserisci come figlio destro
+                nodi[prossimoIndice] = {valore, NODO_VUOTO, NODO_VUOTO};
+                nodi[indice].destro = prossimoIndice++;
+            } else {
+                // Continua la ricerca nel sottoalbero destro
+                inserisciNodo(nodi[indice].destro, valore);
+            }
+        } else {
+            cout << "Il valore " << valore << " esiste già nell'albero.\n";
+        }
+    }
+
+    bool cerca(int valore) const {
+        return cercaNodo(radice, valore);
+    }
+
+    bool cercaNodo(int indice, int valore) const {
+        if (indice == NODO_VUOTO) return false;
+        if (nodi[indice].valore == valore) return true;
+        if (valore < nodi[indice].valore) {
+            return cercaNodo(nodi[indice].sinistro, valore);
+        } else {
+            return cercaNodo(nodi[indice].destro, valore);
+        }
+    }
+
+    void stampaInOrdine() const {
+        stampaInOrdineNodo(radice);
+        cout << endl;
+    }
+
+    void stampaInOrdineNodo(int indice) const {
+        if (indice != NODO_VUOTO) {
+            stampaInOrdineNodo(nodi[indice].sinistro);
+            cout << nodi[indice].valore << " ";
+            stampaInOrdineNodo(nodi[indice].destro);
+        }
+    }
 };
+
+int main() {
+    AlberoBinarioRicerca albero;
+
+    int n, valore;
+    cout << "Quanti numeri vuoi inserire? ";
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cout << "Inserisci un numero: ";
+        cin >> valore;
+        albero.inserisci(valore);
+    }
+
+    cout << "Valori in ordine: ";
+    albero.stampaInOrdine();
+
+    cout << "Inserisci un numero da cercare: ";
+    cin >> valore;
+    if (albero.cerca(valore)) {
+        cout << valore << " trovato nell'albero.\n";
+    } else {
+        cout << valore << " non trovato.\n";
+    }
+
+    return 0;
+}
